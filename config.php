6ddb7ef145d9e51c6b624f8655e53b0a907612f7
Wizular <70189391+wizular@users.noreply.github.com>
2020-08-25 15:21:37 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * @author : SMX Shubham Mohare
+ * @name: Blogging Startup SEO Tools - PHP Script
+ * @copyright © 2018 Blogging Startup
+ *
+ */
+ 
+// Disable Errors
+error_reporting(1);
+
+// Database Hostname
+$dbHost = '';
+
+// Database Username
+$dbUser = '';
+
+// Database Password
+$dbPass = '';
+
+// Database Name
+$dbName = '';
+
+//Item Purchase Code
+$item_purchase_code = 'Blogging-Startup';
+
+//Domain Security Code
+$authCode = '421aa90e079fa326b6494f812ad13e79';
+
+//mod_rewrite
+$mod_rewrite = '1';
+
+?>
